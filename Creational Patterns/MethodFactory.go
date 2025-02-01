46a154a81628a91cec80d57ce0e4f00cebbd1b4f
maruki00 <aitbaha48@gmail.com>
2025-02-01 22:35:58 +0100

    --

---
+++ Creational Patterns/MethodFactory.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var lastId int = 0
+
+type Object struct {
+	Id      int
+	Quatity int
+	Address string
+}
+
+func ObjectFactory(Quatity int, Address string) (*Object, error) {
+	if Quatity <= 0 {
+		return nil, errors.New("invalid quantity")
+	}
+	if Address == "" {
+		return nil, errors.New("invalid address")
+	}
+	lastId++
+	return &Object{
+		Id:      lastId,
+		Quatity: Quatity,
+		Address: Address,
+	}, nil
+}
+
+func main() {
+	obj, err := ObjectFactory(10, "address 1")
+	if err != nil {
+		panic(err)
+	}
+	obj1, err := ObjectFactory(10, "address 1")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("result %#v, \n", obj)
+	fmt.Printf("result %#v, \n", obj1)
+}
