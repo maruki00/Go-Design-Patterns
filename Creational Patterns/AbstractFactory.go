2e9db4440ba01110daf843211d4f4dec43e0f394
maruki00 <aitbaha48@gmail.com>
2025-02-01 21:50:44 +0100

    --

--- Creational Patterns/AbstractFactory.go
+++ Creational Patterns/AbstractFactory.go
@@ -1,49 +1,113 @@
-package main 
+package main
 
-import (
-	"strings"
-)
+import "fmt"
 
-
-type IFactory struct {
+type IFactory interface {
 	MakeShoe() any
 	MakeShirt() any
 }
 
+type IProduct interface {
+	GetLogo() string
+	GetSize() int
+}
 
 func GetInstance(brand string) IFactory {
-	switch  brand {
+	switch brand {
 	case "nike":
-		return &Nike{}	
+		return &Nike{}
 	case "addidas":
 		return &Addidas{}
-	default :
-		return nil
+	default:
+		panic("invalidd type")
 	}
 }
 
-type IProduct interface {
-	GetLogo() string
-	GetSize() int	
+type AddidasShoe struct {
+	Shoe
+}
+
+type AddidasTshirt struct {
+	Tshirt
+}
+
+type NikeShoe struct {
+	Shoe
+}
+
+type NikeTshirt struct {
+	Tshirt
+}
+
+type Addidas struct{}
+
+func (a *Addidas) MakeShoe() any {
+	return &AddidasShoe{
+		Shoe: Shoe{
+			Logo: "addidas shoe",
+			Size: 41,
+		},
+	}
+}
+func (a *Addidas) MakeShirt() any {
+	return &AddidasTshirt{
+		Tshirt: Tshirt{
+			Logo: "addidas tshirt",
+			Size: 5,
+		},
+	}
+}
+
+type Nike struct{}
+
+func (a *Nike) MakeShoe() any {
+	return &NikeShoe{
+		Shoe: Shoe{
+			Logo: "addidas shoe",
+			Size: 41,
+		},
+	}
+}
+func (a *Nike) MakeShirt() any {
+	return &NikeTshirt{
+		Tshirt: Tshirt{
+			Logo: "nike tshirt",
+			Size: 8,
+		},
+	}
+}
+
+type Shoe struct {
+	Logo string
+	// Color string
+	Size int
+}
+
+func (s *Shoe) GetLogo() string {
+	return s.Logo
+}
+func (s *Shoe) GetSize() int {
+	return s.Size
 }
 
 type Tshirt struct {
-	logo string
-	size int
-}
-func (t *Tshirt) getLogo() string{
-	return t.logo
-}  
-func (t *Tshirt) getSize() int	 {
-	return t.size
-}  
-
-
-type Addids struct {
-	
+	Logo string
+	// Color string
+	Size int
 }
 
+func (s *Tshirt) GetLogo() string {
+	return s.Logo
+}
+func (s *Tshirt) GetSize() int {
+	return s.Size
+}
 
+func main() {
+	addidas := GetInstance("addidas")
+	nike := GetInstance("nike")
 
+	fmt.Println("addidas : ", addidas.MakeShirt())
+	fmt.Println("nike : ", nike.MakeShoe())
 
-type 
+}
