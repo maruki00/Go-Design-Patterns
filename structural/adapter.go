7ecdf4ad6f8cf91ed290628cbc8fabd84cb13b01
maruki00 <aitbaha48@gmail.com>
2025-03-12 00:20:12 +0000

    commit

--- structural/adapter.go
+++ structural/adapter.go
@@ -1,1 +1,40 @@
 package structural
+
+import "fmt"
+
+type Data struct {
+	Name string
+	Age  int
+}
+
+type Adapter interface {
+	GetData() string
+}
+
+type JsonAdapter struct {
+	d Data
+}
+
+func NewJsonAdapter(d Data) *JsonAdapter {
+	return &JsonAdapter{
+		d: d,
+	}
+}
+
+func (j *JsonAdapter) GetData() string {
+	return fmt.Sprintf(`{"name": %s, "age": %d}`, j.d.Name, j.d.Age)
+}
+
+type XMLAdapter struct {
+	d Data
+}
+
+func NewXMLAdapter(d Data) *JsonAdapter {
+	return &JsonAdapter{
+		d: d,
+	}
+}
+
+func (x *XMLAdapter) GetData() string {
+	return fmt.Sprintf(`{"name": %s, "age": %d}`, x.d.Name, x.d.Age)
+}
