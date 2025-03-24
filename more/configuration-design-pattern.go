589504a8fabc4205c8aee9408913c454d41b4c30
maruki00 <aitbaha48@gmail.com>
2025-03-24 07:58:45 +0000

    commit

--- more/configuration-design-pattern.go
+++ more/configuration-design-pattern.go
@@ -1,5 +1,32 @@
 package more
 
+type Configuratoin func(s *Server) error
+type Server struct {
+	Host string
+	Port int
+}
 
+func NewServer(cfgs ...Configuratoin) (*Server, error) {
+	s := &Server{}
+	for _, cfg := range cfgs {
+		err := cfg(s)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return s, nil
+}
 
-type Configuratoin func()
+func WithPort(port int) Configuratoin {
+	return func(s *Server) error {
+		s.Port = port
+		return nil
+	}
+}
+
+func WithHost(host string) Configuratoin {
+	return func(s *Server) error {
+		s.Host = host
+		return nil
+	}
+}
